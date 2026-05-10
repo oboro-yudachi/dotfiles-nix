4286d9f2455559817199513ae4b6f81fc8a0c772
shoh13 <10_draft_willful@icloud.com>
2026-05-11 02:47:28 +0900

    update: homebrewからnixpkgsに移行できるやつを移行し、miseをnixで宣言的に管理するようにした

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   home.username = "taguchishoh";
@@ -13,7 +13,30 @@
     ripgrep
     gh
     shellcheck
+    cmake
+    coreutils
+    zstd
+    libtool
+    libyaml
+    rustup
   ];
+
+  programs.mise = {
+    enable = true;
+    enableZshIntegration = true;
+    globalConfig = {
+      settings = {
+        idiomatic_version_file_enable_tools = [ "node" "ruby" ];
+      };
+      tools = {
+        bun = "1.3.11";
+        node = "24.14.1";
+        ruby = "4.0.0";
+        python = "3.14.4";
+        mysql = "8.0";
+      };
+    };
+  };
 
   home.file = {
     ".gitconfig".source = ./git/.gitconfig;
@@ -23,4 +46,9 @@
   };
 
   programs.home-manager.enable = true;
+
+  home.activation.miseInstall = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    ${pkgs.mise}/bin/mise install --quiet
+    ${pkgs.mise}/bin/mise prune --yes
+  '';
 }
--- nix-darwin/homebrew.nix
+++ nix-darwin/homebrew.nix
@@ -21,19 +21,14 @@
 
     brews = [
       "agda"
-      "cmake"
-      "coreutils"
+      # macOS 向け独自パッチ（ネイティブフルスクリーン等）が含まれる特殊 tap のため nixpkgs に移行不可
       "d12frosted/emacs-plus/emacs-plus@30"
+      # 依存関係には現れないが Doom Emacs の起動に必要
       "jpeg"
-      "libtool"
+      # nixpkgs の libvterm は Linux 専用（meta.platforms に aarch64-darwin が含まれない）
       "libvterm"
-      "libyaml"
       "markdown"
-      "mise"
       "mo"
-      "mysql"
-      "rustup"
-      "zstd"
     ];
 
     casks = [
