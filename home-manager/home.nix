b8facceada4afb3a00861f725fd01463d2842003
shoh13 <10_draft_willful@icloud.com>
2026-05-18 00:05:43 +0900

    update: mise.nixを作成してコードを分離する

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,6 +1,10 @@
 { config, pkgs, lib, ... }:
 
 {
+  imports = [
+    ./mise.nix
+  ];
+
   home.username = "taguchishoh";
   home.homeDirectory = "/Users/taguchishoh";
 
@@ -21,23 +25,6 @@
     rustup
   ];
 
-  programs.mise = {
-    enable = true;
-    enableZshIntegration = true;
-    globalConfig = {
-      settings = {
-        idiomatic_version_file_enable_tools = [ "node" "ruby" ];
-      };
-      tools = {
-        bun = "1.3.11";
-        node = "24.14.1";
-        ruby = "4.0.0";
-        python = "3.14.4";
-        mysql = "8.0";
-      };
-    };
-  };
-
   home.file = {
     ".gitconfig".source = ./git/.gitconfig;
   };
@@ -46,9 +33,4 @@
   };
 
   programs.home-manager.enable = true;
-
-  home.activation.miseInstall = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    ${pkgs.mise}/bin/mise install --quiet
-    ${pkgs.mise}/bin/mise prune --yes
-  '';
 }
